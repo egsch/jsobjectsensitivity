c6e1e553c91f1ab0d35d2d5e6bfb648d72913b69
egsch <egschaumann@gmail.com>
2022-07-19 09:57:35 -0500

    add file

---
+++ ObjectSensitivity.js
@@ -0,0 +1,24 @@
+function RandomObject () {
+    this.aMethod = function ( ) {
+        return "6";
+    }
+}
+
+function RandomObject2 () {
+    this.aMethod = function ( ) {
+        return "5";
+    }
+}
+
+function Foo (o) {
+    this.myObject = o;
+    this.doSomething = function () {
+        return this.myObject.aMethod();
+    }
+}
+
+var ab = new RandomObject();
+var cd = new RandomObject2();
+var a1 = new Foo(ab);
+var a2 = new Foo(cd);
+a1.doSomething();
