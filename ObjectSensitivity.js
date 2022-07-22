55a06ddb740720ec093edf98184ae9f877c1059f
Emma Schaumann <61172180+egsch@users.noreply.github.com>
2022-07-22 09:47:53 -0500

    Add duplicate code

--- ObjectSensitivity.js
+++ ObjectSensitivity.js
@@ -22,3 +22,28 @@ var cd = new RandomObject2();
 var a1 = new Foo(ab);
 var a2 = new Foo(cd);
 a1.doSomething();
+
+function RandomObjectA () {
+    this.aMethod = function ( ) {
+        return "6";
+    }
+}
+
+function RandomObjectB () {
+    this.aMethod = function ( ) {
+        return "5";
+    }
+}
+
+function Foo2 (o) {
+    this.myObject = o;
+    this.doSomething = function () {
+        return this.myObject.aMethod();
+    }
+}
+
+var abc = new RandomObjectA();
+var cde = new RandomObjectB();
+var a11 = new Foo2(ab);
+var a22 = new Foo2(cd);
+a11.doSomething();
